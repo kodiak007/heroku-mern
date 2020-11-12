a728bcd47df24d4eb7e6d5acf37df2128da2420a
kodiak007 <makiato.koi@gmail.com>
2020-11-11 20:00:50 -0600

    ajustes para deployment

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json({ extended: true}));
 
 //puerto de la APP
-const PORT = process.env.PORT || 8000;
+const port = process.env.port || 8000;
 
 //importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
@@ -25,7 +25,7 @@ app.use('/api/tareas', require('./routes/tareas'));
 
 
 //arrancar la app
-app.listen(PORT, () =>{
-console.log(`El servidor esta funcionado en el puerto ${PORT} `);
+app.listen(port, '0.0.0.0', () =>{
+console.log(`El servidor esta funcionado en el puerto ${port} `);
 
 });
